9054c11ddf4bb1ad057d26a3053f4b43f5c51766
V4N1LLA-1CE <164747892+V4N1LLA-1CE@users.noreply.github.com>
2024-11-14 01:15:27 +1100

    initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func createHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a dorm for creating a new snippet..."))
+}
+
+func main() {
+	// initialise new router
+	// register homeHandler
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/snippetbox/view", viewHandler)
+	mux.HandleFunc("/snippetbox/create", createHandler)
+
+	// log
+	log.Println("Starting server on :4000")
+
+	if err := http.ListenAndServe(":4000", mux); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module snippetbox.austinsofaer.dev
+
+go 1.23.3
