d73cf79c5a71411524798be231b8ecab25cad663
V4N1LLA-1CE <164747892+V4N1LLA-1CE@users.noreply.github.com>
2024-11-14 19:39:05 +1100

    feat: new endpoints with methods and path variables

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,29 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	msg := fmt.Sprintf("Display a specific snippet with ID %v\n", id)
+	w.Write([]byte(msg))
 }
 
-func createHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a dorm for creating a new snippet..."))
+func createSnippetHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
+
+func createSnippetPostHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Save a new snippet..."))
 }
 
 func main() {
 	// initialise new router
 	// register homeHandler
 	mux := http.NewServeMux()
-	mux.HandleFunc("/{$}", homeHandler) // Restrict route to only match on "/"
-	mux.HandleFunc("/snippetbox/view", viewHandler)
-	mux.HandleFunc("/snippetbox/create", createHandler)
+	mux.HandleFunc("GET /{$}", homeHandler) // Restrict route to only match on "/"
+	mux.HandleFunc("GET /snippetbox/view/{id}", viewHandler)
+	mux.HandleFunc("GET /snippetbox/create", createSnippetHandler)
+	mux.HandleFunc("POST /snippetbox/create", createSnippetPostHandler)
 
 	// log
 	log.Println("Starting server on :4000")
