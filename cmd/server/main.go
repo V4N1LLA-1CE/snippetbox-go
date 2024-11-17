7b413330b6b4953b6683aa02092289652b9b1479
V4N1LLA-1CE <164747892+V4N1LLA-1CE@users.noreply.github.com>
2024-11-17 16:32:05 +1100

    feat: create db connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"log"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/joho/godotenv"
 )
 
 type Application struct {
 	logger *slog.Logger
+}
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("cannot load .env variables: %v\n", err)
+	}
 }
 
 func main() {
@@ -21,6 +32,13 @@ func main() {
 		AddSource: true,
 	}))
 
+	db, err := openPostgresDB(os.Getenv("POSTGRES_URL"))
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
 	// init instance of app struct that contains dependencies
 	app := &Application{
 		logger: logger,
@@ -30,9 +48,25 @@ func main() {
 	logger.Info("starting server...", "addr", *addr)
 
 	// listen on localhost:addr
-	err := http.ListenAndServe(*addr, app.router())
+	err = http.ListenAndServe(*addr, app.router())
 
 	// log the error returned and terminate app
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openPostgresDB(dsn string) (*sql.DB, error) {
+	// initialise db pool
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// establish connection
+	if err := db.Ping(); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
